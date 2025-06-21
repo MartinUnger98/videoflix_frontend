90b80db69ce612cb7b0df82730e4d9044c0a1155
Martin Unger <martin-unger2@live.at>
2025-06-21 19:27:42 +0200

    added primeicons + finished html + css for header and start- page

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-start-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    InputTextModule
+  ],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss'
 })
 export class StartPageComponent {
-
+  primeicons = PrimeIcons;
 }
